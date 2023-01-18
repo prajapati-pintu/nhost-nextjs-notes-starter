98eed4d4bf860bb48e654c49a51d7940185f0022
prajapati-pintu <114797677+prajapati-pintu@users.noreply.github.com>
2023-01-18 19:21:27 +0530

    Update nhost.ts

--- utils/nhost.ts
+++ utils/nhost.ts
@@ -2,8 +2,8 @@ import { NhostClient } from "@nhost/nextjs";
 
 // TODO: Update subdomain and region to your own project.
 const nhost = new NhostClient({
-  subdomain: "absvxxnsesanljntvxui",
-  region: "eu-central-1",
+  subdomain: "mgqsrjjnvjtfarnhvypi",
+  region: "eu-west-2",
 });
 
 export { nhost };
